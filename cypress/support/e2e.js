535e1e93b9eac7a225f91636cedb5e09ae21b2c9
Gabriela <gabriela.szpunar@sky-shop.pl>
2023-04-16 14:16:37 +0200

    percyTest

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -10,11 +10,9 @@
 // 'supportFile' configuration option.
 //
 // You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
 
-// Import commands.js using ES2015 syntax:
 import './commands'
+import '@percy/cypress'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
